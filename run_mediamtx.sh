cde3647198daece422bfc79071eb40e0566ad40d
Alex Sh <shalex.work@gmail.com>
2024-06-23 15:36:42 +0300

    Fix installing mediamtx

--- run_mediamtx.sh
+++ run_mediamtx.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 install() {
     temp_dir=$(mktemp -d)
     wget -P "$temp_dir" https://github.com/bluenviron/mediamtx/releases/download/v1.8.3/mediamtx_v1.8.3_linux_arm64v8.tar.gz
-    mkdir -p ./mediamtx
-    tar -xvf "$temp_dir/mediamtx_v1.8.3_linux_arm64v8.tar.gz" -C "./mediamtx"
+    mkdir -p $SCRIPT_DIR/mediamtx
+    tar -xvf "$temp_dir/mediamtx_v1.8.3_linux_arm64v8.tar.gz" -C "$SCRIPT_DIR/mediamtx"
     rm -rf "$temp_dir"
 }
 
@@ -15,7 +15,7 @@ install_config() {
 }
 
 # Check if Janus is already installed
-if [ ! -f "mediamtx/mediamtx" ]; then
+if [ ! -f "$SCRIPT_DIR/mediamtx/mediamtx" ]; then
     install
 fi
 
