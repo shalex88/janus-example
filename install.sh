c8d034e827e32701abce53231c60a3721de362fd
Alex Sh <shalex.work@gmail.com>
2024-07-09 16:23:26 +0300

    Update mediamtx installation script to support different HW architectures

--- install.sh
+++ install.sh
@@ -4,9 +4,18 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 install() {
     temp_dir=$(mktemp -d)
-    wget -P "$temp_dir" https://github.com/bluenviron/mediamtx/releases/download/v1.8.3/mediamtx_v1.8.3_linux_arm64v8.tar.gz
+
+    if [ "$ARCH" == "aarch64" ]; then
+        arch="arm64v8"
+    elif [ "$ARCH" == "x86_64" ]; then
+        arch="amd64"
+    fi
+
+    file_name="mediamtx_v1.8.3_linux_${arch}.tar.gz"
+    url="https://github.com/bluenviron/mediamtx/releases/download/v1.8.3/$file_name"
+    wget -P "$temp_dir" $url
     mkdir -p $SCRIPT_DIR/mediamtx
-    tar -xvf "$temp_dir/mediamtx_v1.8.3_linux_arm64v8.tar.gz" -C "$SCRIPT_DIR/mediamtx"
+    tar -xvf "$temp_dir/$file_name" -C "$SCRIPT_DIR/mediamtx"
     rm -rf "$temp_dir"
 }
 
